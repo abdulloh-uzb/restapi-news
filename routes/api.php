f6f2f8b84fff49e016b145fed0aa3e1de6015d16
abdulloh-uzb <uabdulloh11@gmail.com>
2024-09-09 19:02:07 +0500

    added routes for comment

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,4 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource("categories", CategoryController::class);
 Route::apiResource("posts", PostController::class);
+Route::apiResource("comments", CommentController::class);
 
