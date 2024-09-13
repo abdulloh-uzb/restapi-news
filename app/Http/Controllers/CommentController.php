2ad85947f7ac711fe91163b95bbfd7b33c70825d
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2024-09-13 14:24:54 +0500

    get post and user comments was done

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCommentRequest;
+use App\Http\Resources\CommentResource;
 use App\Models\Comment;
+use App\Models\Post;
 
 class CommentController extends Controller
 {
@@ -13,5 +15,17 @@ class CommentController extends Controller
         $comment['writer_id'] = 1;
         $comment = Comment::create($comment);
         return response()->json($comment);
+    }
+
+    public function getUserComments()
+    {
+        $comments = Comment::where("writer_id", auth()->id())->get();
+        return new CommentResource($comments);
+    }
+    
+    public function getPostComments(Post $post)
+    {
+        $postComments = Post::with("comments")->find($post->id);
+        return CommentResource::class($postComments->comments);
     }
 }
