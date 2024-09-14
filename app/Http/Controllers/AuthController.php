e28b5fe26846b527c952ac6d49645362979246e7
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2024-09-14 15:49:59 +0500

    added formrequest to register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use App\Mail\EmailConfirmation;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -11,22 +12,13 @@ use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        $validated = $request->validate([
-            "name" =>"required|max:255|min:5",
-            "email" => "required|email|max:255|unique:users,email",
-            "password" => "required|min:6",
-            "password_confirmation" => "same:password",
-            "image" => "nullable|mimes:jpg,jpeg,png",
-        ]);
 
-        $user = User::create($validated);
+        $user = User::create($request->validated());
 
         $code = rand(100000, 999999);
         Mail::to($user)->send(new EmailConfirmation($code));
-
-
 
         return response()->json([
             "message"=> "Confirmation code was sent successfully!",
